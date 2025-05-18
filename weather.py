1f093d8448a5951c40bef818dbbfac8055401d20
Saumya <saumyan24@gmail.com>
2025-05-18 19:50:59 +0530

    commit

--- weather.py
+++ weather.py
@@ -8,7 +8,7 @@ if not api_key:
     print("API Key is missing!")
 
 def get_current_weather(city="Ahmedabad"):
-    request_url=f"https://api.openweathermap.org/data/2.5/weather?appid={os.getenv("API_KEY")}&q={city}&units=imperial"
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?appid={os.getenv('API_KEY')}&q={city}&units=imperial"
     weather_data= requests.get(request_url).json()
     return weather_data
 if __name__ == "__main__":
