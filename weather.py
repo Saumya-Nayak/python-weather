e5fc37fb8b56ac138375a9c0ec1f66002cabe69d
Saumya <saumyan24@gmail.com>
2025-07-29 14:30:52 +0530

    Initial commit with Celsius update

--- weather.py
+++ weather.py
@@ -8,6 +8,9 @@ if not api_key:
     print("API Key is missing!")
 
 def get_current_weather(city="Ahmedabad"):
-    request_url = f"https://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units=imperial"
+
+    request_url = f"https://api.openweathermap.org/data/2.5/weather?appid={api_key}&q={city}&units=metric"
     weather_data = requests.get(request_url).json()
     return weather_data
+
+
